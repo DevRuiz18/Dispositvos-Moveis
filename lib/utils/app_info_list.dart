559a4017397316b05698089a3b3befcfb7cd33ba
DevRuiz18 <gustavosruiz@gmail.com>
2022-10-04 21:33:12 -0300

    N1

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,45 @@
+List<Map<String, dynamic>> movieList = [
+  {
+    'image': 'batman.png',
+    'tittle': 'The Batman',
+  },
+  {
+    'image': 'incantetion.png',
+    'tittle': 'Incantation',
+  },
+  {
+    'image': 'the_lighthouse.png',
+    'tittle': 'The Light House',
+  },
+  {
+    'image': 'your_name.png',
+    'tittle': 'Your Name',
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'tittle': 'Batman',
+    'runtime': '2h 56m',
+    'room': 3,
+    'date': '1 May',
+    'session_time': '18:40 AM',
+    'seat': 'D 27'
+  },
+  {
+    'tittle': 'Avatar',
+    'runtime': '2h 41m',
+    'room': 4,
+    'date': '12 Sep.',
+    'session_time': '21:00 AM',
+    'seat': 'G 36'
+  },
+  {
+    'tittle': 'Akira  ',
+    'runtime': '2h 4m',
+    'room': 8,
+    'date': '27 Jan.',
+    'session_time': '13:00 AM',
+    'seat': 'H 12'
+  },
+];
